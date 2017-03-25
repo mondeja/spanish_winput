d8c4f341258eff524fc79ea04cbde82e4ff5a9e9
mondeja <mondejar1994@gmail.com>
2017-03-25 13:23:08 +0100

    Upload libraries

---
+++ screen.py
@@ -0,0 +1,14 @@
+import numpy as np
+import ImageGrab, ImageOps
+
+# Devuelve la pantalla actual como un objeto
+def screen(save=False, **ruta):
+    im = ImageGrab.grab()
+    if save == True:
+        im.save(ruta, 'PNG')
+    return im
+
+''' OPERACIONES CON LA IMAGEN OBJETO '''
+
+''' Buscar un pixel dadas unas coordenadas: '''
+#print screen().getpixel((x, y))
